6740ef9d9b70ff221a03591ae744d935e5f7db4c
Abhijit5011 <laptopabhi501@gmail.com>
2025-07-20 11:30:01 +0530

    Delete models directory

--- models/Expense.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const expenseSchema = new mongoose.Schema({
-  date: { type: Date, default: Date.now },
-  type: String, // e.g., "raw material", "plastic cups", etc.
-  amount: Number
-});
-
-module.exports = mongoose.model('Expense', expenseSchema);
--- models/Sales.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const saleSchema = new mongoose.Schema({
-  date: { type: Date, default: Date.now },
-  sheetsSold: Number,
-  sellingPrice: Number
-});
-
-module.exports = mongoose.model('Sale', saleSchema);
